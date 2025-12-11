e0a40477921e57e7c28ec2b4984d7ac6663b309e
KingOfMusicBot <teamriya84@gmail.com>
2025-12-11 18:21:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "2762d6b85975700ffddae249e13911cf"
 BOT_TOKEN = getenv("BOT_TOKEN", "8044536901:AAEdh9Pj3iCvEF_r5aKASuXoY9d4VSaQ-to")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = int(getenv("OWNER_ID", 8009369344))
+OWNER_ID = int(getenv("OWNER_ID", 8142003954))
 SUPPORT_GRP = "RIYA_chat_SUPPORT"
 UPDATE_CHNL = "RiyaUpdates"
-OWNER_USERNAME = "ll_hunter_lll"
+OWNER_USERNAME = "ll_KSD_ll"
